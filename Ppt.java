46ec0bde5ed1100c924fe61e2487e3a9f5983fd2
chayi chaithu <chaitanya>
2024-11-03 17:48:02 +0530

    by main

---
+++ Ppt.java
@@ -0,0 +1,8 @@
+public class Ppt{
+    public static void main(String[] args)
+    {
+        System.out.println("Github working presentation");
+        System.out.println("Github working");
+        System.out.println("Github");
+    }
+}
