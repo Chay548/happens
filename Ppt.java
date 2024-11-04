65f510889b68d8611ed05f09b42e3ef2ac288d1b
chayi548 <167800726+Chay548@users.noreply.github.com>
2024-11-04 18:29:46 +0530

    Update Ppt.java

--- Ppt.java
+++ Ppt.java
@@ -2,7 +2,5 @@ public class Ppt{
     public static void main(String[] args)
     {
         System.out.println("Github working presentation");
-        System.out.println("Github working");
-        System.out.println("Github");
     }
 }
